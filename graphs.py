698c0535185d0cf71cad9e165195b7c8c4f6f9ee
Sofiia <sonyprh13.05@gmail.com>
2025-05-10 09:23:25 +0200

    dz graphs

--- graphs.py
+++ graphs.py
@@ -41,3 +41,30 @@ plt.boxplot(data, labels=fruits)
 plt.title("Boxplot of Fruit Weights")
 plt.ylabel("Weight (grams)")
 plt.show()
+
+# 5
+x = np.random.uniform(0, 1, 100)
+y = np.random.uniform(0, 1, 100)
+plt.scatter(x, y, color='green', alpha=0.6)
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.title("Uniformly Distributed Random Points")
+plt.show()
+
+# 6
+x = np.linspace(-2 * np.pi, 2 * np.pi, 500)
+
+f = np.sin(x)
+g = np.cos(x)
+h = np.sin(x) + np.cos(x)
+
+plt.plot(x, f, label='f(x) = sin(x)', color='blue')
+plt.plot(x, g, label='g(x) = cos(x)', color='green')
+plt.plot(x, h, label='h(x) = sin(x) + cos(x)', color='red')
+
+plt.grid(True)
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Graphs of f(x), g(x), and h(x)')
+plt.show()
